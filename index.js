9c8d5e4eb0140e53f00300c859ecf0b32d06cce7
Julie Ralph <ju.ralph@gmail.com>
2015-03-11 15:39:29 -0700

    Grab job ids and logs

--- index.js
+++ index.js
@@ -1,38 +1,116 @@
 var request = require('request');
+var q = require('q');
+var fs = require('fs');
 
 // This is the build number where Socket.io was upgraded. Only go that far back.
-var LAST_BUILD_NUMBER = 16958;
+// var LAST_BUILD_NUMBER = 16958;
+var LAST_BUILD_NUMBER = 17100;
+
+var TRAVIS_HEADERS = {
+  Accept: 'application/vnd.travis-ci.2+json',
+  'User-Agent': 'AngularCiHealth'
+}
 
 var builds = [];
+var jobIds = [];
+
+var jobHistory = {};
+
+var diagnoseJob = function(jobInfo) {
+  var logFile = 'logs/' + jobInfo.id + '.txt'
+  var haveLogDeferred = q.defer();
+
+  if (!fs.existsSync(logFile)) {
+    var writer = fs.createWriteStream(logFile);
+    writer.on('finish', function() {
+      process.stdout.write('-');
+      haveLogDeferred.resolve();
+    });
+
+    request({
+      url: 'http://api.travis-ci.org/jobs/' + jobInfo.id + '/log.txt',
+      headers: {
+        'User-Agent': 'AngularCiHealth',
+        Accept: 'text/plain'
+      }
+    }).on('error', function() {
+      console.log('error');
+      haveLogDeferred.resolve();
+    }).pipe(writer);
+  } else {
+    haveLogDeferred.resolve();
+  }
+
+  return haveLogDeferred.then(function() {
+    
+  });
+};
+
+var getJob = function(jobId) {
+  var deferred = q.defer();
+  request({
+    url: 'http://api.travis-ci.org/jobs/' + jobId,
+    headers: TRAVIS_HEADERS
+  }, function(error, response, body) {
+    process.stdout.write('.');
+    var job = JSON.parse(body).job;
+    if (!jobHistory[job.config.env]) {
+      jobHistory[job.config.env] = [];
+    }
+
+    var jobInfo = {
+      id: job.id,
+      log_id: job.log_id,
+      number: job.number,
+      state: job.state,
+      started_at: job.started_at
+    }
+    jobHistory[job.config.env].push(jobInfo);
+    diagnoseJob(jobInfo).then(deferred.resolve);
+  });
+  return deferred.promise;
+};
+
+var getJobs = function(builds) {
+  for (var i = 0; i < builds.length; ++i) {
+    if (parseInt(builds[i].number) > LAST_BUILD_NUMBER) {
+      jobIds = jobIds.concat(builds[i].job_ids);
+    }
+  }
+  console.dir('Getting job info for ' + jobIds.length + ' jobs');
+  jobPromises = [];
+  for (var j = 0; j < jobIds.length; ++j) {
+    jobPromises.push(getJob(jobIds[j]));
+  }
+  q.all(jobPromises).then(function() {
+    console.dir(jobHistory);
+  });
+};
 
 var getBuilds = function(afterNumber) {
   var qs = {};
   if (afterNumber) {
     qs.after_number = afterNumber;
   }
-
-
   request({
     url: 'http://api.travis-ci.org/repos/angular/angular.js/builds',
     qs: qs,
-    Accept: 'application/vnd.travis-ci.2+json'},
-    function(error, response, body) {
-      if (!error && response.statusCode == 200) {
-        // Seems to always get 25 builds.
-        var newBuilds = JSON.parse(body);
-        // console.dir(newBuilds);
-        builds = builds.concat(newBuilds);
-        if (parseInt(builds[builds.length - 1].number) > LAST_BUILD_NUMBER) {
-          console.dir('Getting 25 more');
-          console.dir(builds[builds.length - 1].number);
-          getBuilds(builds[builds.length - 1].number);
-        } else {
-          console.dir(builds);
-          console.dir('----');
-          console.dir(builds.length);
-        }
+    headers: TRAVIS_HEADERS
+  }, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // Seems to always get 25 builds.
+      var newBuilds = JSON.parse(body).builds;
+      // console.dir(newBuilds);
+      builds = builds.concat(newBuilds);
+      if (parseInt(builds[builds.length - 1].number) > LAST_BUILD_NUMBER) {
+        console.dir('Getting 25 more builds');
+        getBuilds(builds[builds.length - 1].number);
+      } else {
+        console.dir('Got info for ' + builds.length + ' builds');
+        getJobs(builds);
       }
-    })
+    }
+  });
 };
 
 getBuilds(null);
