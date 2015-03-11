e0e513f75ade63c4a38d9e4530965f570acbdc43
Julie Ralph <ju.ralph@gmail.com>
2015-03-10 22:38:43 -0700

    Get all builds up to the specified last number which was the socket.io upgrade.

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 var request = require('request');
 
-request({
-  url: 'http://api.travis-ci.org/repos/angular/angular.js/builds',
-  qs: {
-    'after_number': '17051'
-  },
-  Accept: 'application/vnd.travis-ci.2+json'},
-  function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.dir(JSON.parse(body));
-      console.dir(JSON.parse(body).length);
-    }
-  })
+// This is the build number where Socket.io was upgraded. Only go that far back.
+var LAST_BUILD_NUMBER = 16958;
+
+var builds = [];
+
+var getBuilds = function(afterNumber) {
+  var qs = {};
+  if (afterNumber) {
+    qs.after_number = afterNumber;
+  }
+
+
+  request({
+    url: 'http://api.travis-ci.org/repos/angular/angular.js/builds',
+    qs: qs,
+    Accept: 'application/vnd.travis-ci.2+json'},
+    function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        // Seems to always get 25 builds.
+        var newBuilds = JSON.parse(body);
+        // console.dir(newBuilds);
+        builds = builds.concat(newBuilds);
+        if (parseInt(builds[builds.length - 1].number) > LAST_BUILD_NUMBER) {
+          console.dir('Getting 25 more');
+          console.dir(builds[builds.length - 1].number);
+          getBuilds(builds[builds.length - 1].number);
+        } else {
+          console.dir(builds);
+          console.dir('----');
+          console.dir(builds.length);
+        }
+      }
+    })
+};
+
+getBuilds(null);
