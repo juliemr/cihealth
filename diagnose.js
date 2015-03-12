15a183b06a1d1fc4b139cb6982cfb5c15de9df6d
Julie Ralph <ju.ralph@gmail.com>
2015-03-12 14:40:11 -0700

    Add more potential errors to check

--- diagnose.js
+++ diagnose.js
@@ -1,12 +1,39 @@
 var q = require('q');
 var exec = require('child_process').exec;
 
+
+// TODO - consider looking at the final lines of the output to see which
+// browsers are being problematic.
+
+// Note that the order here matters - the diagnoser will find the first
+// error and report that one, ignoring later matches.
 var STATES = [{
     regexp: 'NoSuchWindowError',
     state: 'NoSuchWindowError'
   }, {
+    regex: 'Disconnected (3 times)',
+    state: 'Karma disconnected more than 2 times'
+  }, {
+    regex: 'WARN \[launcher.browserstack\]: .* has not captured in 60000 ms, killing',
+    state: 'BrowserStack could not capture browser'
+  }, {
+    regexp: 'UnknownCommandError: ERROR Job is not in progress',
+    state: 'Job is not in progress - usually means absolutely everything timed out'
+  }, {
+    regexp: 'UnknownError: Could not start Browser / Emulator',
+    state: 'Could not start Browser / Emulator',
+  }, {
+    regexp: 'No output has been received in the last 10 minutes',
+    state: 'No output received in the last 10 minutes - sometimes tunnel failure'
+  }, {
+    regexp: 'Protractor.waitForAngular()',
+    state: 'Timed out while a spec was waiting for Angular'
+  }, {
     regexp: 'Timed out awaiting response to command \"switchToFrame\"',
     state: 'Timed out switching frames'
+  }, {
+    regexp: 'timeout: timed out after 60000 msec waiting for spec to complete',
+    state: 'A jasmine spec took longer than 60 sec'
   }];
 
 var getGrepCmd = function(logFile, regexp) {
